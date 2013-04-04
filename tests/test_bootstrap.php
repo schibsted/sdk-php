517be1bcd7e8121597d98df13005f48b58701791
Alexander Morland <alek.morland@gmail.com>
2013-04-04 15:00:51 +0200

    testing lint jenkins, commit parse error

--- tests/test_bootstrap.php
+++ tests/test_bootstrap.php
@@ -9,7 +9,7 @@ error_reporting(E_ALL | E_STRICT);
 
 require_once(__DIR__ . '/../src/Client.php');
 
-class BaseIntegrationTest extends PHPUnit_Framework_TestCase {
+class BaseIntegrationTest extends PHPUnit_Framework_TestCase 
 
     public $vgservices;
     public $SPID_CREDENTIALS;
