a2268f5949f231e5e8426e0e48017e32807aaa5f
Alexander Morland <alek.morland@gmail.com>
2013-05-02 10:51:35 +0200

    fixed config and tests for ClientTest where it was set up to go against production and checking the wrong config.

--- tests/unit/ClientTest.php
+++ tests/unit/ClientTest.php
@@ -5,24 +5,36 @@ class ClientTest extends PHPUnit_Framework_TestCase {
 
     public function setUp() {
         $SPID_CREDENTIALS = array(
-            VGS_Client::CLIENT_ID       => '4cf36fa274dea2117e030000',//4d06920474dea26227070000',//
-            VGS_Client::CLIENT_SECRET   => 'foobar',
-            VGS_Client::CLIENT_SIGN_SECRET => 'foobar',
-            VGS_Client::STAGING_DOMAIN  => 'spp.dev',
-            VGS_Client::HTTPS           => false,
-            VGS_Client::REDIRECT_URI    => "http://sdk.dev",
-            VGS_Client::DOMAIN          => 'sdk.dev',
-            VGS_Client::COOKIE          => false,
-            VGS_Client::API_VERSION     => 2,
-            VGS_Client::PRODUCTION      => true,
+            VGS_Client::CLIENT_ID           => '345',
+            VGS_Client::CLIENT_SECRET       => 'foobar',
+            VGS_Client::CLIENT_SIGN_SECRET  => 'foobar',
+            VGS_Client::STAGING_DOMAIN      => 'spp.dev',
+            VGS_Client::HTTPS               => false,
+            VGS_Client::REDIRECT_URI        => "http://sdk.dev",
+            VGS_Client::DOMAIN              => "sdk.dev",
+            VGS_Client::COOKIE              => false,
+            VGS_Client::API_VERSION         => 2,
+            VGS_Client::PRODUCTION          => false,
         );
         $this->client = new VGS_Client($SPID_CREDENTIALS);
     }
 
     public function testClient() {
-        $expected = 'https://payment.schibsted.no';
+        $expected = 'http://spp.dev';
         $result   = $this->client->getServerURL();
         $this->assertEquals($expected, $result);
+
+        $expected = 'sdk.dev';
+        $result   = $this->client->getBaseDomain();
+        $this->assertEquals($expected, $result);
+
+        $this->client->setProduction(true);
+
+        $expected = 'http://payment.schibsted.no';
+        $result   = $this->client->getServerURL();
+        $this->assertEquals($expected, $result);
+
+        $this->client->setProduction(false);
     }
 
     public function testParseSignedRequest() {
