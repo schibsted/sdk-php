404dc991fd55ea470a22f7b8b36b4b886563ed78
yuriykisil <yurij.kisil@schibsted.pl>
2016-05-27 12:57:17 +0200

    change config fo client, to point to local

--- config/dev.php
+++ config/dev.php
@@ -5,11 +5,10 @@ $app['debug'] = true;
 
 // Example overwrites REDIRECT_URI so that they can share this config file
 $app['SPID_CREDENTIALS'] = array(
-
-    VGS_Client::CLIENT_ID       => '4d00e8d6bf92fc8648000000',
+    VGS_Client::CLIENT_ID       => '4e8463569caf7ca019000007',
     VGS_Client::CLIENT_SECRET   => 'foobar',
     VGS_Client::CLIENT_SIGN_SECRET => 'a274de',
-    //VGS_Client::STAGING_DOMAIN  => 'spp.dev',
+    VGS_Client::STAGING_DOMAIN  => 'spp.dev',
     VGS_Client::HTTPS           => false,
     VGS_Client::REDIRECT_URI    => "http://sdk.dev/explorer.php",
     VGS_Client::DOMAIN          => 'sdk.dev',
